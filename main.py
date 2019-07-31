df9db2d9c8f14bfc32904668a621776ee131d2ce
Nicolas Marier <software@nmarier.com>
2019-07-30 21:57:19 -0400

    Turn on an LED on websocket message

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from gpiozero import LED
 from starlette.websockets import WebSocket
 
 app = FastAPI()
@@ -8,5 +9,8 @@ app = FastAPI()
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
     while True:
-        data = await websocket.receive_text()
-        await websocket.send_json({"success": True})
+        data = await websocket.receive_json()
+        if data.get("activate") is True:
+            led = LED(17)
+            led.on()
+            await websocket.send_json({"success": True})
