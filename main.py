94694910c3269c918ef02a6f95a12506fc68a3ba
Nicolas Marier <software@nmarier.com>
2019-07-30 21:50:57 -0400

    Use websockets

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
-
+from starlette.websockets import WebSocket
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World!"}
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_text()
+        await websocket.send_json({"success": True})
