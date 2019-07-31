c7fa00bbfc85a7d0fe3953abf3ac6d96040e0ab7
Nicolas Marier <software@nmarier.com>
2019-07-31 19:18:34 -0400

    Make a method to turn on the LED

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from gpiozero import LED
+from gpiozero.exc import BadPinFactory, GPIOPinInUse
 from starlette.websockets import WebSocket
 
 app = FastAPI()
@@ -11,7 +12,18 @@ async def websocket_endpoint(websocket: WebSocket):
     while True:
         data = await websocket.receive_json()
         if data.get("activate") is True:
-            led = LED(17)
-            if not led.is_active:
-                led.on()
-            await websocket.send_json({"success": True})
+            message = turn_led_on()
+            await websocket.send_json(message)
+
+
+def turn_led_on():
+    try:
+        led = LED(17)
+        if not led.is_active:
+            led.on()
+    except BadPinFactory:
+        return {"success": False, "message": "The current device does not support GPIO operations"}
+    except GPIOPinInUse:
+        pass
+
+    return {"success": True, "message": "The light was turned on"}
